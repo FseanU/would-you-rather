5f5b4bd7139516bc909d374f6a2358712bcbd3b5
Sean Fu <sean14xx@gmail.com>
2020-09-28 12:43:29 +0200

    feat: Question component
    - render author name and avatar

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import Question from './Question'
 
 class Dashboard extends React.Component {
   render() {
-    console.log(this.props)
     return (
       <div>
         <h3>Dashboard</h3>
         <div>
           {this.props.questionIds.map((id) => (
             <li key={id}>
-              Question Id: {id}
+              <Question id={id} />
             </li>
           ))}
         </div>
---
+++ src/components/Question.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { formatQuestion } from '../utils/helpers'
+
+class Question extends React.Component {
+  render() {
+    console.log(this.props)
+    const { question } = this.props
+    if (question === null) {
+      return <p>This Question doesn't exist</p>
+    }
+
+    const { 
+      name, avatar, hasVoted, optionOne
+    } = question
+
+    return (
+      <div>
+        <h3>{`${name} ask:`}</h3>
+        <img 
+          src={avatar} 
+          alt={`Avatar of ${name}`}
+        />
+      </div>
+    )
+  }
+}
+
+function mapStateToProps ({questions, authedUser, users}, { id }) {
+  const question = questions[id]
+
+  return {
+    authedUser,
+    question: question 
+      ? formatQuestion(question, users[question.author], authedUser)
+      : null
+  }
+}
+
+export default connect(mapStateToProps)(Question)
