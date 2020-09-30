d9907684437b815e1a809c1ed7d76ee66c055c00
Sean Fu <sean14xx@gmail.com>
2020-09-30 18:39:27 +0200

    feat: Show loading bar when adding a new question

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,5 +1,5 @@
 import { saveQuestion } from '../utils/api'
-// Add loading bar
+import { showLoading, hideLoading } from 'react-redux-loading'
 
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
 export const ADD_QUESTION = 'ADD_QUESTION'
@@ -15,12 +15,15 @@ export function handleAddQuestion (optionOneText, optionTwoText) {
   return (dispatch, getState) => {
     const { authedUser } = getState()
 
+    dispatch(showLoading())
+
     return saveQuestion({
       optionOneText,
       optionTwoText,
       author: authedUser,
     })
       .then((question) => dispatch(addQuestion(question)))
+      .then(() => dispatch(hideLoading()))
   }
 }
 
