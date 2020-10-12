ee052ca5c93d7e2b0fa906551f80db0afc7b09da
Sean Fu <sean14xx@gmail.com>
2020-10-12 20:31:33 +0200

    feat: Add style to NewQuestion

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -41,29 +41,33 @@ class NewQuestion extends React.Component {
     }
 
     return (
-      <div>
-        <h2>Create New Question</h2>
-        <p>Complete the question:</p>
-        <h3>Would you rather...</h3>
-        <form onSubmit={this.handleSubmit}>
+      <div className="form-container mt-56">
+        <h1>Create New Question</h1>
+        {/* <p>Complete the question:</p> */}
+        <h3 className="mt-16">Would you rather...</h3>
+        <form className="mt-24" 
+              onSubmit={this.handleSubmit}>
           <textarea 
             name='optionOne'
             placeholder='Enter Option One Text Here'
             value={optionOne}
             onChange={this.handleInputChange}
             maxLength={200}
+            className="mt-8 p-8"
           />
-          <p>OR</p>
+          <p className="mt-8">OR</p>
           <textarea 
             name="optionTwo"
             placeholder='Enter Option Two Text Here' 
             value={optionTwo}
             onChange={this.handleInputChange}
             maxLength={200}
+            className="mt-8 p-8"
           />
           <button
             type='submit'
-            disabled={optionOne === '' || optionTwo === ''}>
+            disabled={optionOne === '' || optionTwo === ''}
+            className="mt-40">
             Submit
           </button>
         </form>
