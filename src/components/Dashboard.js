bda86839a55f6d96576ba297e6ff810188257be8
Sean Fu <sean14xx@gmail.com>
2020-10-19 14:15:15 +0200

    feat: Add style to dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { makeStyles } from '@material-ui/core/styles';
 import { Tabs, Tab } from '@material-ui/core'
+import Grid from '@material-ui/core/Grid'
 import Question from './Question'
 import { isQuestionVoted } from '../utils/helpers'
 
+const useStyles = makeStyles({
+  indicator: {
+    backgroundColor: "#ff7257",
+  },
+  buttonStyle: {
+    color: "#ff7257",
+  }
+});
+
 function Dashboard (props) {
   const [selectedTab, setSelectedTab] = React.useState(0);
+  const classes = useStyles(selectedTab);
 
   const handleChange = (event, newValue) => {
     setSelectedTab(newValue);
@@ -22,22 +34,31 @@ function Dashboard (props) {
   })
 
   return (
-    <div>
-      <h3>Dashboard</h3>
-      <Tabs value={selectedTab} onChange={handleChange} >
-        <Tab label="Unanswered Questions" />
-        <Tab label="Answered Questions" />
-      </Tabs>
-      {selectedTab === 0 && unansweredQuestionIds.map((id) => (
-        <li key={id}>
-          <Question id={id} />
-        </li>
-      ))}
-      {selectedTab === 1 && answeredQuestionIds.map((id) => (
-        <li key={id}>
-          <Question id={id} />
-        </li>
-      ))}
+    <div className="dashboard mt-72">
+      <Grid 
+        container
+        justify="center"
+      >
+        <Tabs 
+          value={selectedTab} 
+          onChange={handleChange} 
+          TabIndicatorProps={{ className: classes.indicator }} >
+          <Tab label="Unanswered Questions" className={selectedTab === 1 ? '' : classes.buttonStyle} />
+          <Tab label="Answered Questions" className={selectedTab === 0 ? '' : classes.buttonStyle} />
+        </Tabs>
+      </Grid>
+      <div className="mt-16 dashboard-cards">
+        {selectedTab === 0 && unansweredQuestionIds.map((id) => (
+          <li key={id}>
+            <Question id={id} />
+          </li>
+        ))}
+        {selectedTab === 1 && answeredQuestionIds.map((id) => (
+          <li key={id}>
+            <Question id={id} />
+          </li>
+        ))}
+      </div>
     </div>
   )
 }
