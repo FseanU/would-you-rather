4c9d33c946fda24521430b659b8b8708161ef504
Sean Fu <sean14xx@gmail.com>
2020-10-20 11:39:36 +0200

    fix: Update user state after created a question

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,5 +1,5 @@
 import { RECEIVE_USERS } from '../actions/users'
-import { ANSWER_QUESTION } from '../actions/questions'
+import { ANSWER_QUESTION, ADD_QUESTION } from '../actions/questions'
 
 export default function users (state={}, action) {
   switch(action.type) {
@@ -17,6 +17,14 @@ export default function users (state={}, action) {
             ...state[action.authedUser].answers,
             [action.qid]: action.answer
           }
+        }
+      }
+    case ADD_QUESTION : 
+      return {
+        ...state,
+        [action.question.author]: {
+          ...state[action.question.author],
+          questions: state[action.question.author].questions.concat([action.question.id])
         }
       }
     default :
