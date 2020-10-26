c8b0cd2c00f07db62e0f43fefc2b7a6ce19b080b
Sean Fu <sean14xx@gmail.com>
2020-10-26 14:55:31 +0100

    feat: Add style to 404 page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 export default function NotFound(props) {
   return (
-    <div>
+    <div className="mt-72 not-found-container">
       <img 
         src={require(`../images/404_image.png`)} 
         alt="404 illustration"
       />
-      <p>The page you were looking for doesn’t exist</p>
+      <h2>The page you were looking for doesn’t exist.</h2>
     </div>
   )
 }
