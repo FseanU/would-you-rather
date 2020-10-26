1ff3477ab4f62dbc9f549a83ece8455a45b40150
Sean Fu <sean14xx@gmail.com>
2020-10-26 15:01:04 +0100

    feat: Display navbar while not sign in

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -11,52 +11,53 @@ function Nav (props) {
 
   return (
     <nav className="nav">
-      {user ? 
-        (<React.Fragment>
-          <ul>
-            <div className="nav-route">
-              <li id="logo-li">
-                <NavLink id="logo" to='/' exact>
-                  WYR
-                </NavLink>
-              </li>
-              <li>
-                <NavLink 
-                  to='/' exact 
-                  className={pathName === '/' ? 'black' : ''}>
-                  Dashboard
-                </NavLink>
-              </li>
-              <li>
-                <NavLink 
-                  to='/new'
-                  className={pathName === '/new' ? 'black' : ''}>
-                  New Question
-                </NavLink>
-              </li>
-              <li>
-                <NavLink 
-                  to='/leaderboard'
-                  className={pathName === '/leaderboard' ? 'black' : ''}>
-                  Leader Board
-                </NavLink>
-              </li>
-            </div>
-            <div className="nav-user">
-              <li>
-                <img src={require(`../${avatar}`)} alt="Your avatar"/>
-                <p className="pl-8">{name}</p>
-              </li>
-              <li>
-                <NavLink to='/logout'>
-                  Logout
-                </NavLink>
-              </li>            
-            </div>
-          </ul>
-          {/* <div className="line-bottom-black"></div> */}
-        </React.Fragment>)
-        : ''
+      <React.Fragment>
+        <ul>
+          <div className="nav-route">
+            <li id="logo-li">
+              <NavLink id="logo" to='/' exact>
+                WYR
+              </NavLink>
+            </li>
+            <li>
+              <NavLink 
+                to='/' exact 
+                className={pathName === '/' ? 'black' : ''}>
+                Dashboard
+              </NavLink>
+            </li>
+            <li>
+              <NavLink 
+                to='/new'
+                className={pathName === '/new' ? 'black' : ''}>
+                New Question
+              </NavLink>
+            </li>
+            <li>
+              <NavLink 
+                to='/leaderboard'
+                className={pathName === '/leaderboard' ? 'black' : ''}>
+                Leader Board
+              </NavLink>
+            </li>
+          </div>
+          {user
+            ? (<div className="nav-user">
+                <li>
+                  <img src={require(`../${avatar}`)} alt="Your avatar"/>
+                  <p className="pl-8">{name}</p>
+                </li>
+                <li>
+                  <NavLink to='/logout'>
+                    Logout
+                  </NavLink>
+                </li>            
+              </div>)
+            : ''
+          }
+        </ul>
+      </React.Fragment>
+        
       }
     </nav>
   )
